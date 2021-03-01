f5ba78a84f598872dfd38fb008b792ef17c23cf9
Weera Y <acike@live.com>
2021-03-01 15:32:15 +0700

    Jacoco

--- src/main/java/com/example/kbtg/KbtgApplication.java
+++ src/main/java/com/example/kbtg/KbtgApplication.java
@@ -1,13 +1,27 @@
 package com.example.kbtg;
 
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KbtgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KbtgApplication.class, args);
+	// IoC Container => Context, Bean  => Spring
+		// IoC (Inversion of Control)
+		// DI (Dependency Injection)
+		ConfigurableApplicationContext context
+				= SpringApplication.run(KbtgApplication.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+		for (String bean : beans) {
+			System.out.println(bean);
+		}
+		System.out.println(context.getBeanDefinitionCount());
+
 	}
+
+
 
 }
