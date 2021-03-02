3a12e30ecd9dbf351676b27196ab8933edf825c9
Weera Y <acike@live.com>
2021-03-02 11:22:09 +0700

    WO3-Lombok

--- src/main/java/com/example/kbtg/user/UserResponse.java
+++ src/main/java/com/example/kbtg/user/UserResponse.java
@@ -1,45 +1,16 @@
 package com.example.kbtg.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
 
 public class UserResponse {
     private Integer id;
     private String name;
     private Integer age;
-
-    public UserResponse() {
-    }
-
-    public UserResponse(Integer id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
 
 }
